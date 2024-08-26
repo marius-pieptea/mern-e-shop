743092d709d14f52bbf772480ebefaf797cfd607
Mariuspieptea1 <marius.pieptea11@gmail.com>
2024-08-26 18:56:36 +0300

    add retry to server

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 const path = require("path");
 
-const userRoutes = require('./routes/userRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const productRoutes = require('./routes/productRoutes');
+const userRoutes = require("./routes/userRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const productRoutes = require("./routes/productRoutes");
 
-const setupSwagger = require('./swagger');
-const setupInMemoryDB = require('./setupInMemoryDb'); // Adjust the path as necessary
+const setupSwagger = require("./swagger");
+const setupInMemoryDB = require("./setupInMemoryDb"); // Adjust the path as necessary
 
 dotenv.config();
 
@@ -19,25 +19,31 @@ app.use(express.json());
 
 setupSwagger(app);
 
-// Routes 
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/products', productRoutes);
-
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-const connectDB = async () => {
-  try {
-    // await mongoose.connect(process.env.MONGO_URI);
-        console.log("Setting up inmemory MongoDB");
-    await setupInMemoryDB();
-  } catch (error) {
-    console.error(
-      "Failed to setup in-memory database",
-      error
-    );
-
+const connectDB = async (retries = 5, delay = 5000) => {
+  while (retries) {
+    try {
+      console.log("Setting up in-memory MongoDB");
+      await setupInMemoryDB();
+      console.log("In-memory MongoDB setup successfully");
+      break;
+    } catch (error) {
+      console.error("Failed to setup in-memory database", error);
+      retries -= 1;
+      console.log(`Retries left: ${retries}`);
+      if (retries === 0) {
+        console.error("Exhausted all retries. Exiting...");
+        process.exit(1);
+      }
+      console.log(`Retrying in ${delay / 1000} seconds...`);
+      await new Promise((res) => setTimeout(res, delay));
+    }
   }
 };
 
@@ -45,8 +51,8 @@ connectDB();
 
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-app.get('/', (req, res) => {
-    res.send('API is running...');    
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
